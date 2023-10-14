5de859a3b11ca024990751582ec39c7ed0a59942
Lolit <lolitmohan090@gmail.com>
2023-10-14 21:19:20 +0600

    fast

--- src/services/cartServices.js
+++ src/services/cartServices.js
@@ -1,27 +1,14 @@
+const CartModel=require('../model/CartModel');
 const mongoose = require("mongoose");
-const ProductModel = require("../models/ProductModel");
-const CartModel = require("../models/CartModel");
 const ObjectId = mongoose.Types.ObjectId;
 
 const CreateCart = async (req)=>{
     try{
         let user_id=req.headers.id;
-        let reqBody=req.body;
-        let productID=reqBody.productID;
-
-        // Price Calculation
-        let product= await ProductModel.findOne({_id:productID});
-        let price=product.price;
-        if(product.discount){
-            price=product.discountPrice;
-        }
-        let totalPrice=price*reqBody.qty;
-
+        let reqBody=req.body;// Product ID
         reqBody.userID = user_id;
-        reqBody.price = totalPrice;
-
         await  CartModel.updateOne({userID: user_id, productID: reqBody.productID}, {$set:reqBody}, {upsert:true})
-        return {status:"success", message:"Cart List Created"}
+        return {status:"success", message:"Wish List Created"}
     }
     catch (e) {
         return {status:"fail", message:"Something Went Wrong"}
@@ -34,7 +21,7 @@ const RemoveCart = async (req)=>{
         let reqBody=req.body;// Product ID
         reqBody.userID = user_id;
         await  CartModel.deleteOne({userID: user_id, productID: reqBody.productID})
-        return {status:"success", message:"Cart Item Deleted"}
+        return {status:"success", message:"Wish List Deleted"}
     }
     catch (e) {
         return {status:"fail", message:"Something Went Wrong"}
@@ -43,40 +30,29 @@ const RemoveCart = async (req)=>{
 
 const Cart = async (req)=>{
     try{
-
         let user_id=new ObjectId(req.headers.id);
-
         let matchStage= {$match: {userID:user_id}}
-        let JoinStageProduct={$lookup: {from: "products", localField: "productID", foreignField: "_id", as: "product"}};
-        let unwindProductStage={$unwind: "$product"}
 
-        let JoinStageBrand={$lookup: {from: "brands", localField: "product.brandID", foreignField: "_id", as: "brand"}};
-        let unwindBrandStage={$unwind: "$brand"}
+        let JoinStageProduct={$lookup:{from:'products', localField:'productID', foreignField:'_id', as:'product'}}
+        let unwindProduct={$unwind:"$product"}
 
-        let JoinStageCategory={$lookup: {from: "categories", localField: "product.categoryID", foreignField: "_id", as: "category"}};
-        let unwindCategoryStage={$unwind: "$category"}
-        let projectionStage= {$project: {'_id': 0,
-                'userID': 0, 'createdAt':0,
-                'updatedAt':0,'product._id':0,
-                'product.categoryID':0,'product.brandID':0,
-                'brand._id':0,'category._id':0,
-        }}
+        let JoinStageCategory={$lookup:{from:'categories', localField:'product.categoryID', foreignField:'_id', as:'category'}}
+        let unwindCategory={$unwind: "$category"}
+
+        let JoinStageBrand={$lookup:{from:'brands', localField:'product.brandID', foreignField:'_id', as:'brand'}}
+        let unwindBrand={$unwind: '$brand'}
+
+        let Projection={$project:{'_id':0, 'userID':0, 'createdAt':0, 'updatedAt':0, 'product._id':0, 'product.categoryID':0, 'product.brandID':0, 'category._id':0, 'brand._id':0}}
 
         let data= await CartModel.aggregate([
-            matchStage,
-            JoinStageProduct,
-            unwindProductStage,
-            JoinStageBrand,
-            unwindBrandStage,
-            JoinStageCategory,
-            unwindCategoryStage,
-            projectionStage
-        ])
-
+            matchStage, JoinStageProduct, unwindProduct, JoinStageCategory, unwindCategory, JoinStageBrand, unwindBrand, Projection])
         return {status:"success", data:data}
     }
     catch (e) {
         return {status:"fail", message:"Something Went Wrong"}
     }
 }
-module.exports = {CreateCart,RemoveCart,Cart}
+
+
+
+module.exports={CreateCart, RemoveCart, Cart}
